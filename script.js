81a4a622a16bf781cd5380c606bce1089070b440
maria-jon <mariajonasson93@gmail.com>
2024-11-20 13:16:53 +0100

    add function to print producs

--- script.js
+++ script.js
@@ -41,4 +41,32 @@ const products = [
         alt: 'Stack of donuts'
       },
     },
-  ];
+];
+
+// ------------ HTML ELEMENTS ------------
+  const productsListDiv = document.querySelector('#products-list');
+
+// ------------ PRINT PRODUCTS IN HTML ------------
+
+function printProductsList() {
+    // Rensa div:en på befintliga produkter innan utskrift av uppdaterad information
+    productsListDiv.innerHTML = '';
+  
+    products.forEach(product => {
+      productsListDiv.innerHTML += `
+        <article class="product">
+          <h3>${product.name}</h3>
+          <p>${product.price} kr</p>
+          <p>Rating: ${getRatingHtml(product.rating)}</p>
+          <img src="${product.img.url}" alt="${product.img.alt}">
+          <div>
+            <button class="decrease" id="decrease-${product.id}">decrease</button>
+            <input type="number" min="0" value="${product.amount}" id="input-${product.id}">
+            <button class="increase" id="increase-${product.id}">increase</button>
+          </div>
+        </article>
+      `;
+    });
+}
+
+printProductsList();
