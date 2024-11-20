e04f0e15a7df5e6c325d70c5fca71132625c7585
maria-jon <mariajonasson93@gmail.com>
2024-11-20 13:09:26 +0100

    add products

---
+++ script.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+      id: 0,
+      name: 'Product 1',
+      price: 10,
+      rating: 4,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: 'assets/images/assets/images/annie-spratt-jstQCOhzyQA-unsplash.jpg',
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 1,
+      name: 'Product 2',
+      price: 20,
+      rating: 3,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: 'assets/images/elena-koycheva-PFzy4N0_R3M-unsplash.jpg',
+        width: 1000,
+        height: 450,
+        alt: 'Two donuts'
+      },
+    },
+    {
+      id: 2,
+      name: 'Product 3',
+      price: 30,
+      rating: 1.5,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: 'assets/images/assets/images/annie-spratt-jstQCOhzyQA-unsplash.jpg',
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+  ];
