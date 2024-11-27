311efa083021e2fa6f84d8f93b33460f037b3a45
maria-jon <mariajonasson93@gmail.com>
2024-11-27 14:39:03 +0100

    :truck: move products array to module

---
+++ products.mjs
@@ -0,0 +1,144 @@
+const products = [
+    {
+      id: 0,
+      name: 'Jordgubbsmagi',
+      price: 10,
+      rating: 4,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 1,
+      name: 'Chokladfrossa',
+      price: 20,
+      rating: 3,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: "./images/elena-koycheva-PFzy4N0_R3M-unsplash.jpg",
+        width: 500,
+        height: 500,
+        alt: 'Two donuts'
+      },
+    },
+    {
+      id: 2,
+      name: 'Honungsglädje',
+      price: 30,
+      rating: 1.5,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 3,
+      name: 'Regnbågsring',
+      price: 35,
+      rating: 4.5,
+      amount: 0,
+      category: 'sour',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 4,
+      name: 'Kanelfluff',
+      price: 20,
+      rating: 2.5,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 5,
+      name: 'Sockersöt',
+      price: 10,
+      rating: 3,
+      amount: 0,
+      category: 'vegan',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 6,
+      name: 'Vaniljdröm',
+      price: 30,
+      rating: 3.5,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 7,
+      name: 'Karamellsnurra',
+      price: 35,
+      rating: 4,
+      amount: 0,
+      category: 'vegan',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 8,
+      name: 'Päronpärla',
+      price: 15,
+      rating: 2,
+      amount: 0,
+      category: 'vegan',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+    {
+      id: 9,
+      name: 'Citronkyss',
+      price: 25,
+      rating: 4.5,
+      amount: 0,
+      category: 'sour',
+      img: {
+        url: "./images/annie-spratt-jstQCOhzyQA-unsplash.jpg",
+        width: 1000,
+        height: 450,
+        alt: 'Stack of donuts'
+      },
+    },
+  ];
+
+export default products;
